0efdccafee398e6502de958a81a05ae8ce642f67
Alex Zaitsev <saizewalex@gmail.com>
2023-02-15 20:58:12 +0300

    Edited build.gradle; edited JUnit4, JUnit Jupiter tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,14 +1,15 @@
 package ru.netology.service;
 
 import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 
 public class CashbackHackServiceTest {
 
     CashbackHackService service = new CashbackHackService();
 
-    @Test
+    // tests JUnit4
+    @org.junit.Test
     public void shouldSuggest1() {
 
         int actual = service.remain(999);
@@ -17,7 +18,8 @@ public class CashbackHackServiceTest {
         Assert.assertEquals(expected, actual);
     }
 
-    @Test
+
+    @org.junit.Test
     public void shouldSuggest0() {
 
         int actual = service.remain(1000);
@@ -26,7 +28,7 @@ public class CashbackHackServiceTest {
         Assert.assertEquals(expected, actual);
     }
 
-    @Test
+    @org.junit.Test
     public void shouldSuggest999IfAmountMore1000() {
 
         int actual = service.remain(1001);
@@ -35,7 +37,7 @@ public class CashbackHackServiceTest {
         Assert.assertEquals(expected, actual);
     }
 
-    @Test
+    @org.junit.Test
     public void shouldSuggest999IfAmountLess1000() {
 
         int actual = service.remain(1);
@@ -44,7 +46,7 @@ public class CashbackHackServiceTest {
         Assert.assertEquals(expected, actual);
     }
 
-    @Test
+    @org.junit.Test
     public void shouldSuggest500IfAmountLess1000() {
 
         int actual = service.remain(500);
@@ -53,7 +55,7 @@ public class CashbackHackServiceTest {
         Assert.assertEquals(expected, actual);
     }
 
-    @Test
+    @org.junit.Test
     public void shouldSuggest200IfAmountMore1000() {
 
         int actual = service.remain(2800);
@@ -62,7 +64,7 @@ public class CashbackHackServiceTest {
         Assert.assertEquals(expected, actual);
     }
 
-    @Test
+    @org.junit.Test
     public void shouldSuggest0IfTheAmountIsAMultipleOfTheBoundary() {
 
         int actual = service.remain(4000);
@@ -70,4 +72,71 @@ public class CashbackHackServiceTest {
 
         Assert.assertEquals(expected, actual);
     }
+
+    // tests JUnit Jupiter
+
+    @org.junit.jupiter.api.Test
+    public void shouldSuggest1Jupiter() {
+
+        int actual = service.remain(999);
+        int expected = 1;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldSuggest0Jupiter() {
+
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldSuggest999IfAmountMore1000Jupiter() {
+
+        int actual = service.remain(1001);
+        int expected = 999;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldSuggest999IfAmountLess1000Jupiter() {
+
+        int actual = service.remain(1);
+        int expected = 999;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldSuggest500IfAmountLess1000Jupiter() {
+
+        int actual = service.remain(500);
+        int expected = 500;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldSuggest200IfAmountMore1000Jupiter() {
+
+        int actual = service.remain(2800);
+        int expected = 200;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldSuggest0IfTheAmountIsAMultipleOfTheBoundaryJupiter() {
+
+        int actual = service.remain(4000);
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
 }
